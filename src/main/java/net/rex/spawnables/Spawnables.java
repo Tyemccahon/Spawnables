6bda4d9a7cc27e27d3553ce52e5941f300969f0c
Rex <tyemccahon3@gmail.com>
2022-10-01 14:04:54 +1000

    Test

---
+++ src/main/java/net/rex/spawnables/Spawnables.java
@@ -0,0 +1,15 @@
+package net.rex.spawnables;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Spawnables implements ModInitializer {
+	public static final String MOD_ID = "spawnables";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/rex/spawnables/SpawnablesClient.java
@@ -0,0 +1,10 @@
+package net.rex.spawnables;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpawnablesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
